f0c5c1c7734514c113e474ade1719de3a9f29451
yangdan4 <daniely@andrew.cmu.edu>
2023-01-28 11:01:56 -0600

    add source data

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import {ChartComponent} from './Components/ChartComponent';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <ChartComponent />
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
